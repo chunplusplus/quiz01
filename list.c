73720795dbb9537c5a3619574f69dfd32b0683d8
johnny <mrchunchun@hotmail.com>
2021-03-03 09:32:29 +0800

    Improve capacity by current index reuse

--- list.c
+++ list.c
@@ -5,14 +5,14 @@
 #include "list.h"
 
 static inline void list_add_node_t(node_t **list, node_t *node_t) {
-    node_t->next = *list;
-    *list = node_t;
+  node_t->next = *list;
+  *list = node_t;
 }
 
 static inline void list_concat(node_t **left, node_t *right) {
-    while (*left)
-      left = &((*left)->next);
-    *left = right;
+  while (*left)
+    left = &((*left)->next);
+  *left = right;
 }
 
 void list_free(node_t **list) {
@@ -33,91 +33,80 @@ node_t *list_make_node_t(node_t *list, int number) {
   return node;
 }
 
-void quicksort(node_t **list)
-{
-      if (!*list)
-          return;
-      
-      #define MAX_LEVELS 300
-      void *beg[MAX_LEVELS], *end[MAX_LEVELS];
-      beg[0] = *list;
-      end[0] = NULL;
+void quicksort(node_t **list) {
+  if (!*list)
+    return;
 
-      int cur_index = -1;
-      int last_index = 0;
+#define MAX_LEVELS 300
+  void *beg[MAX_LEVELS], *end[MAX_LEVELS];
+  beg[0] = *list;
+  end[0] = NULL;
 
-      printf("init value \n");
+  int cur_index = 0;
 
-      do {
-          cur_index++;
-          node_t *pivot = (node_t*) beg[cur_index];
-          int value = pivot->value;
-          
-	  printf("pivot value: %d\n", value);
-          node_t *p = pivot->next;
-          pivot->next = NULL;
+  do {
+    node_t *pivot = (node_t *)beg[cur_index];
+    int value = pivot->value;
 
-          node_t *left = NULL, *right = NULL;
-          
-	  node_t *left_tail = NULL;
-	  node_t *right_tail = NULL;
+    // printf("cur_index: %d, pivot value: %d\n", cur_index, value);
+    node_t *p = pivot->next;
+    pivot->next = NULL;
 
-	  while (p != end[cur_index]) {
+    node_t *left = NULL, *right = NULL;
 
-              node_t *n = p;
-              p = p->next;
-              list_add_node_t(n->value > value ? &right : &left, n);
+    node_t *left_tail = NULL;
+    node_t *right_tail = NULL;
 
-	      if (n->value > value) {
-	          if (right_tail == NULL) right_tail = n;
-	      } 
-	      else {
-	          if (left_tail == NULL) left_tail = n;
-	      }
-          }
-/*
-	  if (left && left->next) {
-	  
-	      last_index++;
-	      beg[last_index] = left;
-	      end[last_index] = left_tail;
-	  }
-	  if (right && right->next) {
-              last_index++;
-	      beg[last_index] = right;
-	      end[last_index] = right_tail;
-	  }
-*/
-          node_t *result = NULL;
-          
-	  if (*list != pivot) {
-              node_t *list_head = *list;
-	      while (list_head->next && list_head->next != pivot) list_head = list_head->next;
-	      list_head->next = NULL;
-	      list_concat(&result, *list);
-          }
+    // quick sort in the range
+    while (p != end[cur_index]) {
 
+      node_t *n = p;
+      p = p->next;
 
-          list_concat(&result, left);
-          list_concat(&result, pivot);
-          list_concat(&result, right);
+      if (n->value > value) {
+        list_add_node_t(&right, n);
+        if (right_tail == NULL)
+          right_tail = n;
+      } else {
+        list_add_node_t(&left, n);
+        if (left_tail == NULL)
+          left_tail = n;
+      }
+    }
+    node_t *result = NULL;
 
-          if (p) list_concat(&result, p);
+    // cut the list to pivot and concat to result
+    if (*list != pivot) {
+      node_t *list_head = *list;
+      while (list_head->next && list_head->next != pivot)
+        list_head = list_head->next;
+      list_head->next = NULL;
+      list_concat(&result, *list);
+    }
 
+    list_concat(&result, left);
+    list_concat(&result, pivot);
+    list_concat(&result, right);
 
-          if (left && left->next) {
-	      last_index++;
-	      beg[last_index] = left;
-	      end[last_index] = left_tail->next;
-	  }
+    // concat the rest of the list
+    if (p)
+      list_concat(&result, p);
 
-	  if (right && right->next) {
-	      last_index++;
-	      beg[last_index] = right;
-	      end[last_index] = right_tail->next;
-	  }
+    cur_index--;
 
-          *list = result;
+    if (left && left->next) {
+      cur_index++;
+      beg[cur_index] = left;
+      end[cur_index] = left_tail->next;
+    }
 
-      } while (cur_index != last_index);
+    if (right && right->next) {
+      cur_index++;
+      beg[cur_index] = right;
+      end[cur_index] = right_tail->next;
+    }
+
+    *list = result;
+
+  } while (cur_index >= 0);
 }
